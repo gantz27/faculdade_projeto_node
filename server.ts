e69d2acc79285974b7882e61f481dcead3e10e36
gantz27 <bielguedes484@gmail.com>
2021-08-23 20:56:41 -0300

    segunda aula do projeto node da faculdade

--- server.ts
+++ server.ts
@@ -1,12 +1,45 @@
 const express = require('express')
 const app = express ()
 
+app.use(express.json())
+
 app.get("/", function(req, res) {
     res.send("primeira chamada")
 })
 
 app.get("/teste", function(req, res) {
-    res.send("chamando api chamada TESTE")
+    res.send("chamando api chamada TESTE - segunda aula...")
+})
+
+app.get("/teste/:username", function(req, res) {
+    const { username } = req.params
+    
+    res.send({
+        "message": "testando método GET com passagem de parâmetros",
+        "username": username
+    })
+
+   //res.status(500).json({ error: 'username not found'})
+
+})
+
+app.post("/teste", function(req, res){
+  //  res.send({
+  //      "message": "testando metodo POST"
+  //  })
+
+    const { nome, sobrenome} = req.body
+
+    if (nome == undefined || sobrenome == undefined) {
+        res.status(500).json({ error: 'Bad Request'})
+    }
+
+    res.send({
+        "message": "testando método POST",
+        "nome": nome,
+        "sobrenome": sobrenome
+    })
+
 })
 
 app.listen(3333, () => console.log("Servidor rodando na porta 3333"))
